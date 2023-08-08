eac845d44ef0a2044b6696a336c79b7c7ef47d3e
Aashish240192 <aashish240192@gmail.com>
2023-08-08 22:50:19 +0530

    Update 01 May 2023 Bdd project files

---
+++ src/test/java/StepDefinition/Base.java
@@ -0,0 +1,17 @@
+package StepDefinition;
+
+import org.openqa.selenium.WebDriver;
+
+import pageObject.Admin;
+import utilities.ReadConfig;
+
+public class Base {
+
+	public WebDriver driver;
+	
+	public Admin ad;
+	
+	public ReadConfig readConfig;
+	
+
+}
